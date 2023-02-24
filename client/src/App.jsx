0b92fa4c15f13d418870154921b0f06ec8a2dc32
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-24 16:37:40 +0100

    Setup Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import UpdateBook from "./pages/UpdateBook";
 
 function App() {
   const [data, setData] = useState("");
@@ -17,7 +21,14 @@ function App() {
     fetchData();
   }, []);
 
-  return <div className="App">{data}</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/books" replace={true} />} />
+      <Route path="/books" element={<Home />} />
+      <Route path="/books/add" element={<AddBook />} />
+      <Route path="/books/update/:id" element={<UpdateBook />} />
+    </Routes>
+  );
 }
 
 export default App;
