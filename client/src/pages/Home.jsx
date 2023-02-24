a3812709417f405e0567c9c87a5d8d86df07a43f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-24 18:43:15 +0100

    Create home layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="App">
+      <header className="header">
+        <h1 className="title">Books</h1>
+        <Link className="add-book-btn">Add New Book</Link>
+      </header>
+      <main className="main"></main>
     </div>
   );
 };
