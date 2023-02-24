21368bbaeeaeb45164cda173a781e52a34ad949e
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-24 18:48:55 +0100

    Move data fetching to books component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,10 @@
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AddBook from "./pages/AddBook";
 import UpdateBook from "./pages/UpdateBook";
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get("http://localhost:8800");
-        setData(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- client/src/components/Books.jsx
+++ client/src/components/Books.jsx
@@ -1,7 +1,24 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Books = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800");
+        setData(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Books</h1>
+      <h1>{data}</h1>
     </div>
   );
 };
