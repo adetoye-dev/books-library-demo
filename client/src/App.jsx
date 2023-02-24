32671cc6574325838e3fe2ecff8c4d7e97808ccc
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-24 15:48:49 +0100

    Fetch data from api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">Hello From App</div>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800");
+        setData(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <div className="App">{data}</div>;
 }
 
 export default App;
