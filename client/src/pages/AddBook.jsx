445e8f5d33988cfffa51695b8a35dd1f02ec6cdb
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-25 04:43:57 +0100

    Create function to handle adding new book

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -2,13 +2,32 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddBook = () => {
+  const navigate = useNavigate();
+
   const [book, setBook] = useState({
     title: "",
     desc: "",
     cover: "",
   });
+
+  const addBook = async () => {
+    try {
+      const response = await axios.post("http://localhost:8800/books", {
+        ...book,
+      });
+
+      if (response.status == 200) {
+        alert("Book added successfully");
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleChange = (e) => {
     setBook((book) => ({ ...book, [e.target.name]: e.target.value }));
@@ -49,7 +68,9 @@ const AddBook = () => {
             name="cover"
             onChange={handleChange}
           />
-          <Button variant="contained">Add Book</Button>
+          <Button variant="contained" onClick={addBook}>
+            Add Book
+          </Button>
         </Box>
       </main>
     </>
