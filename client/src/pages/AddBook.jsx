633b6ef4209822537cd7ae105cb5e65e01c6db7a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-25 03:58:06 +0100

    Create add book form

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -1,8 +1,58 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+
 const AddBook = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    cover: "",
+  });
+
+  const handleChange = (e) => {
+    setBook((book) => ({ ...book, [e.target.name]: e.target.value }));
+  };
+
   return (
-    <div>
-      <h1>AddBook</h1>
-    </div>
+    <>
+      <header className="header justify-center">
+        <h1 className="title">Add New Book</h1>
+      </header>
+      <main className="main">
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="book-title"
+            label="Book Title"
+            variant="outlined"
+            name="title"
+            onChange={handleChange}
+          />
+          <TextField
+            id="book-desc"
+            label="Book Desc"
+            variant="outlined"
+            name="desc"
+            onChange={handleChange}
+          />
+          <TextField
+            id="book-cover"
+            label="Book Cover"
+            variant="outlined"
+            name="cover"
+            onChange={handleChange}
+          />
+          <Button variant="contained">Add Book</Button>
+        </Box>
+      </main>
+    </>
   );
 };
 
