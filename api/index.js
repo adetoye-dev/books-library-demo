470f8b505c3b70b24b40de5a31db5cffb07ba4db
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-25 04:32:07 +0100

    Create backend api request handlers

--- api/index.js
+++ api/index.js
@@ -25,6 +25,37 @@ app.get("/books", (req, res) => {
   });
 });
 
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO test.books (`title`, `desc`, `cover`) VALUES (?)";
+
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const q =
+    "UPDATE test.books SET title = '', desc = '', cover = '' WHERE id = ?";
+
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [...values, req.params.id], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const q = "DELETE FROM test.books WHERE id = ?";
+  db.query(q, req.params.id, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 const port = process.env.PORT || 8800;
 
 app.listen(port, () => {
